f451ee88b740e7c01cea86f6846ea5cc91081d33
Agni-AIT <agni.muhammad@akarinti.tech>
2023-02-22 11:57:53 +0700

    create api request, model api request, and on progress build api reqeust

--- wubbalubbadubdub/APIClient/WLDDRequest.swift
+++ wubbalubbadubdub/APIClient/WLDDRequest.swift
@@ -8,5 +8,54 @@
 import Foundation
 
 final class WLDDRequest {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+
+    private let endpoint: WLDDEndPoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return nil
+    }
+    
+    // MARK: - Public
+    
+    init(
+        endpoint: WLDDEndPoint,
+        pathComponent: [String] = [],
+        queryParameter: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponent = pathComponent
+        self.queryParameter = queryParameter
+    }
     
 }
